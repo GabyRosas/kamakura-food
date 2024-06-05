1ffad11ce9148f9bb53b6dc55b0283ac24e98b98
LauraGDev <laura.gilcara@gmail.com>
2024-06-05 16:25:19 +0200

    Little tweak to adapt the activateFilter to the new functions

--- src/searcher.js
+++ src/searcher.js
@@ -1,14 +1,16 @@
 import { products } from "../assets/data/data.js";
-//import { showProducts } from "./menu"
+import { addProductContainer } from "./menu"
 //DEBE buscar los productos por los filtros
 
 function activateFilter(e) {
     let filter = e.target.innerText;
     let products = filterProducts(filter);
-    showProducts(products); // adaptar a funcion de kate
+    addProductContainer(products);
 }
 
 function filterProducts(cat) {
     let filteredProducts = products.filter( p => p.category.includes(cat));
     return filteredProducts;
 }
+
+export { activateFilter }
