c32cfb31c64f148c05c3fcd2fbc360699378646e
LauraGDev <laura.gilcara@gmail.com>
2024-06-05 15:16:48 +0200

    Adds functionality that filters products by category.

--- src/searcher.js
+++ src/searcher.js
@@ -1,3 +1,14 @@
+import { products } from "../assets/data/data.js";
+//import { showProducts } from "./menu"
 //DEBE buscar los productos por los filtros
 
+function activateFilter(e) {
+    let filter = e.target.innerText;
+    let products = filterProducts(filter);
+    showProducts(products); // adaptar a funcion de kate
+}
 
+function filterProducts(cat) {
+    let filteredProducts = products.filter( p => p.category.includes(cat));
+    return filteredProducts;
+}
