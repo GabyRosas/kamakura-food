d70f7d19936379a059804dc82bec6223fb451291
GabyRosas <fleuryrosas@gmail.com>
2024-06-05 16:52:34 +0200

    not ok

--- src/events.js
+++ src/events.js
@@ -1,2 +1,6 @@
 //Intenta separar los eventos en este archivo.
 
+import { showButtons } from "./menu.js";
+
+let shButton = document.getElementById("filters");
+shButton.addEventListener("click", showButtons)
--- src/menu.js
+++ src/menu.js
@@ -1,3 +1,26 @@
+import { filters } from "../assets/data/data.js";
+
 //DEBE imprimir en pantalla la información de filtros.
 
+const menuButtons = document.getElementById("filters");
+const filtersContainer = document.querySelector("filters-container");
+const nameButton = document.querySelector("filters");
+
+function showButtons(filters) {
+     
+    for (let i = 0; i<filters.length; i++){
+
+        const menuButton = filtersContainer.createElement("button");
+        nameButton.innerHTML = filters[i]; 
+        console.log(nameButton)
+
+        menuButtons.appendChild(menuButton)
+        console.log(filter[i])
+    }   
+
+}
+showButtons(filters); 
+
 //DEBE imprimir en pantalla los productos, con su Título, descripción y precio en € y botón de añadir.
+
+export { showButtons }
