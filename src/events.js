9b7ea5645eabfe5ca4db1d1f18a2f662a847e376
LauraGDev <laura.gilcara@gmail.com>
2024-06-06 13:04:52 +0200

    Adds listeners to the filters and fixes the bug that appeared when clicking on 'todos'

--- src/events.js
+++ src/events.js
@@ -1,9 +1,18 @@
 //Intenta separar los eventos en este archivo.
-
 import { showButtons, addProductContainer } from "./menu.js";
+import { activateFilter } from "./searcher.js";
 import { products } from "../assets/data/data.js"
+
+const filters = document.getElementsByClassName("filter");
 
 document.addEventListener('DOMContentLoaded',(event) =>{
     showButtons();
-    addProductContainer(products)
+    addFilterEvents();
+    addProductContainer(products);
 });
+
+function addFilterEvents(){
+    for (let filter of filters) {
+        filter.addEventListener("click", activateFilter);
+    }
+}
