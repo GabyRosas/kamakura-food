cf566b950c33ffc0afa907e314b725bfa3fa34f1
BCN-Asumiko <yakoshko@gmail.com>
2024-06-06 15:44:51 +0200

    change function show all items

--- src/cart.js
+++ src/cart.js
@@ -1,2 +1,25 @@
 //DEBE contener las funcionalidades del carrito de compras.
 
+//Exportar event listener a su fichero
+
+
+const addButton = document.querySelectorAll(".add-button");
+const cartProductsContainer = document.getElementById("cart-products");
+const cartProduct = document.querySelector(".cart-container");
+const cartTextContainer = document.querySelector(".text-container");
+const cartProductName = document.querySelector(".text-container").children[0];
+const cartSubtotal = document.querySelector(".text-container").children[1];
+
+cartProductsContainer.removeChild(cartProduct)
+
+console.log(addButton)
+
+function addProduct () {
+    console.log("hi")
+
+
+    //Condicion - comprobar que que no esta y anadir
+
+    //else llamar la funcion de aumentar
+
+}
