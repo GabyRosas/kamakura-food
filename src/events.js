d435bc0a822d5d14f50e7bb4b12b8222a578e1de
castellanorn <castellanorn@gmail.com>
2024-06-08 01:51:44 +0200

    add function open and close receipt

--- src/events.js
+++ src/events.js
@@ -1,16 +1,21 @@
 //Intenta separar los eventos en este archivo.
 import { showButtons, addProductContainer } from "./menu.js";
 import { activateFilter } from "./searcher.js";
-import {openCart} from "./cart.js";
+import {openCart } from "./cart.js";
+import {openReceipt} from "./receipt.js";
 import { products } from "../assets/data/data.js"
 
 const filters = document.getElementsByClassName("filter");
+let buttonProceedPay= document.getElementById("proceedPay-button");
+let buttonClose= document.getElementById("close-receipt");
+
 
 document.addEventListener('DOMContentLoaded',(event) =>{
     showButtons();
     addFilterEvents();
     addProductContainer(products);
     addCartEvent();
+    addReceiptEvent();
 });
 
 function addFilterEvents(){
@@ -22,4 +27,13 @@ function addFilterEvents(){
 function addCartEvent(){
     let buttonCart = document.getElementById("cart");
         buttonCart.addEventListener("click", openCart);
-    }
+}
+
+function addReceiptEvent() {
+    buttonProceedPay.addEventListener("click", openReceipt);
+    buttonClose.addEventListener("click", openReceipt);
+}
+
+
+
+
--- src/receipt.js
+++ src/receipt.js
@@ -1,5 +1,25 @@
 //Aquí intenta poner las funcionalidades del recibo
 
+console.log("holaa")
+
+let windowReceipt = document.getElementById("receipt-container");
+let windowCart = document.getElementById("products-container");
+console.log(windowReceipt);
+
+
+function openReceipt () {
+    if ( windowReceipt.style.display === "none" == true) {
+        windowReceipt.style.display = "flex";
+        windowCart.style.display = "none";
+    } else {
+        windowReceipt.style.display = "none";
+        windowCart.style.display = "flex";
+    }
+};
+
+
 function manageReceipt(e) {
     
 }
+
+export {openReceipt}
