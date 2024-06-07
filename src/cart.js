0ace91a36e5616de8b8db4b613688dfe2c546b53
GabyRosas <fleuryrosas@gmail.com>
2024-06-07 12:10:35 +0200

    function create subtotal ok

--- src/cart.js
+++ src/cart.js
@@ -1,2 +1,14 @@
 //DEBE contener las funcionalidades del carrito de compras.
 
+function subTotals (price, subtotal, operator) {       
+    let subTotal;
+if (operator == '+'){
+    subTotal = price + subtotal;
+   } else {
+    subTotal = price - subtotal; 
+}
+return subTotal;
+}
+
+
+export { subTotals }
